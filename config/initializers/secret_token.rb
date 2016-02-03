78d891090664cbc44e91789dbea58021ab5a904e
ATJohnson <arubyrailsdev@gmail.com>
2016-02-03 20:41:08 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rooknroll::Application.config.secret_key_base = 'b3ee30481defcce3f26b5834013f1ce3548530b1b7a2cd1abf4be4bcd8f6556bb63dcdcfce18de82660c3228aff605ceb13c1ed7e11f13bd2e770a6d9fe8994e'

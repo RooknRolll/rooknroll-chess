43fc181a564616216d0eee7f777a0bf2cc3c782b
Nathan <nathangelliott@gmail.com>
2016-02-05 23:07:52 +0000

    Added Devise for Player authentication

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'rails_12factor', group: :production
 
+gem 'devise', '>= 3.2.4'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rooknroll::Application.configure do
+  config.action_mailer.default_url_options = { host: 'rooknroll.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

bd0dc31f26771d2fee5a8a424eb6feab3343a4a7
Andrew Brink <abrink27@gmail.com>
2016-02-07 23:02:17 +0000

    games controller added

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+	def index	
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def show
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rooknroll::Application.routes.draw do
   devise_for :players
 root 'static_pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

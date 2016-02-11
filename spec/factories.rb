21b4c4cb293178967f18c4d58108b8be79dfb649
ATJohnson <arubyrailsdev@gmail.com>
2016-02-11 20:38:01 +1100

    Added Rspec and FactoryGirl and wrote a simple test to check the landing page was loading correctly.

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :user do
+    sequence :username do |n|
+      "tets#{n}"
+    end
+    sequence :email do |n|
+      "dummyEmail#{n}@gmail.com"
+    end
+    password "secretPassword"
+    password_confirmation "secretPassword"
+
+  end
+end

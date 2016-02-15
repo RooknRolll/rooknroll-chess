0e866f949438f18b70424c7b8340063db7ef0f65
ATJohnson <arubyrailsdev@gmail.com>
2016-02-15 11:07:57 +0000

    fixed typo in model/game.rb file

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ActiveRecord::Base
   belongs_to :white_player, class_name: 'Player'
   has_many :pieces
 
-  delegate :kings, :pawns, :rooks, :queens, :knights, :bishops to: :pieces
+  delegate :kings, :pawns, :rooks, :queens, :knights, :bishops, to: :pieces
   # Using deligate allows methods like, game.pawns, game.kings, game.rooks, game.pieces
 
 end

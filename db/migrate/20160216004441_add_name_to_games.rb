a6af19b5858f97c7516937b680e2aabb79327909
dino217 <brob217@gmail.com>
2016-02-15 16:52:34 -0800

    I added a missing migration. When I merged Andrew's pull request there were two migrations that created the games table. I deleted Andrews. I then created a new migration to add the names column in, but I forgot to add it before commiting and pushing.

---
+++ db/migrate/20160216004441_add_name_to_games.rb
@@ -0,0 +1,5 @@
+class AddNameToGames < ActiveRecord::Migration
+  def change
+    add_column :games, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214201550) do
+ActiveRecord::Schema.define(version: 20160216004441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

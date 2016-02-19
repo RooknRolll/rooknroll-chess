a18b2b42564d23deb65ddeaf8a7d44d408d2dafe
dino217 <brob217@gmail.com>
2016-02-18 21:02:49 -0800

    Now able to move pieces. There are still no validations happening prior to the move.

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -6,7 +6,20 @@ class PiecesController < ApplicationController
     @game = @chosen_piece.game
   end
 
+  def update
+    @piece = Piece.find(params[:id])
+    # We still need to validate the move. Right now you can do pretty much
+    # anything. The validation methods should be written in the model, and
+    # called here.
+    @piece.update(piece_params)
+    redirect_to game_path(@piece.game)
+  end
+
   private
+
+  def piece_params
+    params.permit(:x_coordinate, :y_coordinate)
+  end
 
   helper_method :chosen
 

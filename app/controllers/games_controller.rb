58fdeebb4d1e15c9a531673cab0ab1f0819c8ffa
Nathan <nathangelliott@gmail.com>
2016-02-19 19:33:54 +0000

    Added requests directory to spec directory

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,29 @@
 class GamesController < ApplicationController
+  before_action :authenticate_player!
+  before_action :set_game, only: [:show, :update]
+
   def index
-    @game = Game.new
+    
   end
 
   def new
+    @game = Game.new
   end
 
   def create
   end
 
   def show
-    @game = Game.find(params[:id])
     @pieces = @game.pieces.order(:y_coordinate, :x_coordinate)
+  end
+
+  def update
+    @game.black_player_id = current_player
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 end
---
+++ spec/requests/join_game_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Join a Game", type: :request do
+  it "works! (now write some real specs)" do
+    player = create(:player)
+    game = create(:game, black_player_id: nil)
+    post player_session_path, player:{login: player.username, password: "secretPassword"}
+    # get join_games_path
+    # expect(response).to have_http_status(200)
+    # get game index
+    put game_path(game)
+  end
+end

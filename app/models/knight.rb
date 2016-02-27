366193517b2a03a1ce1b2ecb487a08627bdeb13a
Nathan <nathangelliott@gmail.com>
2016-02-27 22:40:31 +0000

    Added tests for knight model spec

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,6 +1,13 @@
 # Class for Knight Piece
 class Knight < Piece
-  def valid_move?(x_coordinate, y_coordinate)
-    self.guard_move_is_on_board?(x_coordinate, y_coordinate)
+  def valid_move?(x, y)
+    false if self.guard_move_is_on_board?(x, y) == false
+    move_x = (x - self.x_coordinate).abs
+    move_y = (y - self.y_coordinate).abs
+    unless move_x == 2 && move_y == 1 || move_x == 1 && move_y == 2
+      false
+    else
+      true
+    end
   end
 end
--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -1,14 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Knight, type: :model do
-  before(:each) do
-    @game = create(:game)
-    @game.pieces.destroy_all
-    @queen = create(:queen, game_id: @game.id)
-    @knight = create(:knight, game_id: @game.id)
-  end
-  it 'raises an error when the move destination is off the board' do
-    expect { @knight.valid_move?(8, 2) }.to raise_error(
-      StandardError, 'The given coordinates are not on the board')
+  describe 'valid_move?' do    
+    before(:each) do
+      @game = create(:game)
+      @game.pieces.destroy_all
+      @queen = create(:queen, game_id: @game.id)
+      @knight = create(:knight, game_id: @game.id, color: 'White', x_coordinate: 1, y_coordinate: 0)
+    end
+
+    context 'invalid move' do
+      it 'raises an error when the move is off the board' do
+        expect { @knight.valid_move?(8, 2) }.to raise_error(
+          StandardError, 'The given coordinates are not on the board')
+      end
+
+      it 'is false if a same team piece is in the destination' do
+        @queen.x_coordinate = 0
+        @queen.y_coordinate = 2
+        expect(@knight.valid_move?(0, 2)).to eq(false)
+      end
+    end
+
+    context 'valid move' do
+      it 'is true for a move 1 left, 2 down' do
+        expect(@knight.valid_move?(0, 2)).to eq(true)
+      end
+
+      it 'is true for a move 1 right, 2 down' do
+        expect(@knight.valid_move?(1, 2)).to eq(true)
+      end
+
+      it 'is true for a move 2 right, 1 down' do
+        expect(@knight.valid_move?(3, 1)).to eq(true)
+      end
+    end
   end
 end

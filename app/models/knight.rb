d28d4292aaeb541bbffc315edb024ad9ec58d26f
Nathan <nathangelliott@gmail.com>
2016-02-27 23:25:24 +0000

    Updated the knight model and spec

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,9 +1,10 @@
 # Class for Knight Piece
 class Knight < Piece
-  def valid_move?(x, y)
-    false if self.guard_move_is_on_board?(x, y) == false
-    move_x = (x - self.x_coordinate).abs
-    move_y = (y - self.y_coordinate).abs
+  def valid_move?(x_new, y_new)
+    return false if guard_move_is_on_board?(x_new, y_new) == false
+    return false if move_attacking_own_piece?(x_new, y_new, color)
+    move_x_new = (x - self.x_coordinate).abs
+    move_y_new = (y - self.y_coordinate).abs
     unless move_x == 2 && move_y == 1 || move_x == 1 && move_y == 2
       false
     else
--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -5,34 +5,42 @@ RSpec.describe Knight, type: :model do
     before(:each) do
       @game = create(:game)
       @game.pieces.destroy_all
-      @queen = create(:queen, game_id: @game.id)
-      @knight = create(:knight, game_id: @game.id, color: 'White', x_coordinate: 1, y_coordinate: 0)
+      @knight = create(:knight, game_id: @game.id, x_coordinate: 1, y_coordinate: 0)
     end
 
-    context 'invalid move' do
+    context 'an invalid move' do
       it 'raises an error when the move is off the board' do
         expect { @knight.valid_move?(8, 2) }.to raise_error(
           StandardError, 'The given coordinates are not on the board')
       end
 
-      it 'is false if a same team piece is in the destination' do
-        @queen.x_coordinate = 0
-        @queen.y_coordinate = 2
+      it 'returns false when the destination is the same place' do
+        expect(@knight.valid_move?(1, 0)).to eq(false)
+      end
+
+      it 'returns false if a same color piece is in the destination' do
+        create(:pawn, x_coordinate: 0, y_coordinate: 2, game_id: @game.id)
         expect(@knight.valid_move?(0, 2)).to eq(false)
       end
     end
 
-    context 'valid move' do
-      it 'is true for a move 1 left, 2 down' do
+    context 'a valid move' do
+      it 'returns true for a move 1 left, 2 down to an unoccupied space' do
         expect(@knight.valid_move?(0, 2)).to eq(true)
       end
 
-      it 'is true for a move 1 right, 2 down' do
-        expect(@knight.valid_move?(1, 2)).to eq(true)
+      it 'returns true for a move 1 right, 2 down to an unoccupied space' do
+        expect(@knight.valid_move?(2, 2)).to eq(true)
       end
 
-      it 'is true for a move 2 right, 1 down' do
+      it 'returns true for a move 2 right, 1 down to an unoccupied space' do
         expect(@knight.valid_move?(3, 1)).to eq(true)
+      end
+
+      it 'returns true when it moves to a space with an opponent piece' do
+        create(:pawn, x_coordinate: 0, y_coordinate: 2, color: 'Black',
+                    game_id: @game.id)
+        expect(@knight.valid_move?(0, 2)).to eq(true)
       end
     end
   end

0a5299c4cb2efbd307467ac311a740b0a14ca639
Nathan <nathangelliott@gmail.com>
2016-02-27 23:30:23 +0000

    Updated knight model and specs

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -3,8 +3,8 @@ class Knight < Piece
   def valid_move?(x_new, y_new)
     return false if guard_move_is_on_board?(x_new, y_new) == false
     return false if move_attacking_own_piece?(x_new, y_new, color)
-    move_x_new = (x - self.x_coordinate).abs
-    move_y_new = (y - self.y_coordinate).abs
+    move_x = (x_new - self.x_coordinate).abs
+    move_y = (y_new - self.y_coordinate).abs
     unless move_x == 2 && move_y == 1 || move_x == 1 && move_y == 2
       false
     else

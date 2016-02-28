4be0395315fc002cb58d3449afbf4d7e7e408109
Andrew Brink <abrink27@gmail.com>
2016-02-28 03:54:04 +0000

    final

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,10 +1,10 @@
 # Class for Rook Piece
 class Rook < Piece
   def valid_move?(new_x, new_y)
-  	return false unless move_is_NSEW(new_x, new_y)
-  	return false if is_obstructed?(new_x, new_y)
-  	return false if move_attacking_own_piece?(new_x, new_y, color)
-  	true
+    return false unless move_is_NSEW(new_x, new_y)
+    return false if is_obstructed?(new_x, new_y)
+    return false if move_attacking_own_piece?(new_x, new_y, color)
+    true
   end
 
   def castle

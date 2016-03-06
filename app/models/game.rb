da7eb09b363428181062aa33ae193d9b910e751e
dino217 <brob217@gmail.com>
2016-03-06 09:41:25 -0800

    Switched placement of king and queen pieces.

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,13 +9,13 @@ class Game < ActiveRecord::Base
   delegate :kings, :pawns, :rooks, :queens, :knights, :bishops, to: :pieces
   # Using delegate allows methods like, game.pawns
   # game.kings, game.rooks, game.pieces
-  # Cassie: I think we can use this for the populate_board! below, but not sure. 
+  # Cassie: I think we can use this for the populate_board! below, but not sure.
 
   def self.with_open_seats
     Game.where('white_player_id IS :nil or black_player_id IS :nil', nil: nil)
   end
 
-  # Populate the board 
+  # Populate the board
   def populate_board!
     # // Black Pieces
 
@@ -23,8 +23,8 @@ class Game < ActiveRecord::Base
     pieces.create(:type => 'Rook', :color => "Black", :x_coordinate => 0, :y_coordinate => 7)
     pieces.create(:type => 'Knight', :color => "Black", :x_coordinate => 1, :y_coordinate => 7)
     pieces.create(:type => 'Bishop', :color => "Black", :x_coordinate => 2, :y_coordinate => 7)
-    pieces.create(:type => 'Queen', :color => "Black", :x_coordinate => 3, :y_coordinate => 7)
-    pieces.create(:type => 'King', :color => "Black", :x_coordinate => 4, :y_coordinate => 7)
+    pieces.create(:type => 'King', :color => "Black", :x_coordinate => 3, :y_coordinate => 7)
+    pieces.create(:type => 'Queen', :color => "Black", :x_coordinate => 4, :y_coordinate => 7)
     pieces.create(:type => 'Bishop', :color => "Black", :x_coordinate => 5, :y_coordinate => 7)
     pieces.create(:type => 'Knight', :color => "Black", :x_coordinate => 6, :y_coordinate => 7)
     pieces.create(:type => 'Rook', :color => "Black", :x_coordinate => 7, :y_coordinate => 7)
@@ -39,13 +39,13 @@ class Game < ActiveRecord::Base
     # Pawns Row from left to right
     (0..7).each do |p|
     	pieces.create(:type => 'Pawn', :color => "White", :x_coordinate => p, :y_coordinate => 1)
-    end 
+    end
     # Back Row from left to right
     pieces.create(:type => 'Rook', :color => "White", :x_coordinate => 0, :y_coordinate => 0)
     pieces.create(:type => 'Knight', :color => "White", :x_coordinate => 1, :y_coordinate => 0)
     pieces.create(:type => 'Bishop', :color => "White", :x_coordinate => 2, :y_coordinate => 0)
-    pieces.create(:type => 'Queen', :color => "White", :x_coordinate => 3, :y_coordinate => 0)
-    pieces.create(:type => 'King', :color => "White", :x_coordinate => 4, :y_coordinate => 0)
+    pieces.create(:type => 'King', :color => "White", :x_coordinate => 3, :y_coordinate => 0)
+    pieces.create(:type => 'Queen', :color => "White", :x_coordinate => 4, :y_coordinate => 0)
     pieces.create(:type => 'Bishop', :color => "White", :x_coordinate => 5, :y_coordinate => 0)
     pieces.create(:type => 'Knight', :color => "White", :x_coordinate => 6, :y_coordinate => 0)
     pieces.create(:type => 'Rook', :color => "White", :x_coordinate => 7, :y_coordinate => 0)

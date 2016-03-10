0fd7c35a3db5ba853682676170d3cf1c7b6ea917
Nathan <nathangelliott@gmail.com>
2016-03-10 15:56:04 +0000

    Added tests for the king being checked by different pieces

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -19,8 +19,61 @@ RSpec.describe Game, type: :model do
   end
 
   describe "check?" do
-    it 'should return false if the King is not in check' do
-      false
+    before(:each) do
+      @game = create(:game)
+      @game.pieces.destroy_all
+      @king = create(:king, game_id: @game.id, x_coordinate: 4, y_coordinate: 0, color: 'White')
+    end
+
+    context 'Pawn' do
+      it 'should return true if the opponent Pawn can capture the King' do
+        @pawn = create(:pawn, game_id: @game.id, x_coordinate: 3, y_coordinate: 1, color: 'Black')
+      end
+    end
+
+    context 'Knight' do
+      it 'should return true if the opponent Knight can capture the King' do
+        @knight = create(:knight, game_id: @game.id, x_coordinate: 3, y_coordinate: 2, color: 'Black')
+      end
+    end
+
+    context 'Rook' do
+      before(:each) do
+        @rook = create(:rook, game_id: @game.id, x_coordinate: 4, y_coordinate: 2, color: 'Black')
+      end
+
+      it 'should return true if the opponent Rook can capture the King' do
+      end
+
+      it 'should return false if the Rook is blocked by another piece' do
+        @pawn = create(:pawn, game_id: @game.id, x_coordinate: 4, y_coordinate: 1, color: 'White')
+      end
+    end
+
+    context 'Bishop' do
+      before(:each) do
+        @bishop = create(:bishop, game_id: @game.id, x_coordinate: 2, y_coordinate: 2, color: 'Black')
+      end
+
+      it 'should return true if the opponent Bishop can capture the King' do
+      end
+
+      it 'should return false if the Bishop is blocked by another piece' do
+        @pawn = create(:pawn, game_id: @game.id, x_coordinate: 3, y_coordinate: 1, color: 'White')
+      end
+    end
+
+    context 'Queen' do
+      before(:each) do
+        @queen = create(:queen, game_id: @game.id, x_coordinate: 2, y_coordinate: 2, color: 'Black')
+      end
+
+      it 'should return true if the opponent Queen can capture the King' do
+      end
+
+      it 'should return false if the Queen is blocked by another piece' do
+        @pawn = create(:pawn, game_id: @game.id, x_coordinate: 3, y_coordinate: 1, color: 'White')
+      end
     end
   end
 end

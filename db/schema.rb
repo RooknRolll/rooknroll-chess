d147f436952f7351a0313d5fd072007ea0576a1a
Nathan <nathangelliott@gmail.com>
2016-03-18 17:39:29 +0000

    Added a turn column to the Games table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311040620) do
+ActiveRecord::Schema.define(version: 20160318172210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160311040620) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.integer  "turn",            default: 0
   end
 
   create_table "pieces", force: true do |t|
--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe PiecesController, type: :controller do
-
+  
+  # describe "pieces#update action" do
+  #   it "should increase the game turn value by 1" do
+  #     game = create(:game)
+  #     pieces.update
+  #     expect(turn).to eq 1
+  #   end
+  # end
 end

35ea43626512cd682677bedc6bb2eccd732e5fcd
Andrew Brink <abrink27@gmail.com>
2016-04-10 02:10:20 +0000

    log in page prettified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+	
+	def resource_name
+		:player
+	end
+
+	def resource
+		@resource ||= Player.new
+	end
+
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:player]
+	end
+
+	def resource_class
+  		devise_mapping.to
+	end
 end
